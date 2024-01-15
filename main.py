f62748e52b53160d7e1c0c3ef1fb6ffb7b77e308
pre5ent666 <75908533+pre5ent666@users.noreply.github.com>
2024-01-15 18:07:08 +0800

    added treeview of clients info. in page3

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
+"""
+Ref.:
+- https://www.pythontutorial.net/tkinter/tkinter-treeview/
+- 
+"""
 import tkinter as tk
 from tkinter import ttk
+from src.tk_items import Page
+from src.database_client import Database_Cli
 
 LARGEFONT = ("Arial", 20)
-
-class Page(tk.Frame):
-    def __init__(self, *args, **kwargs):
-        tk.Frame.__init__(self, *args, **kwargs)
-    def show(self):
-        self.lift()
-
+USERS_DATA = "./db/users_data.db"
 class New_record(Page):
     pg_name = "單日記帳"
 
@@ -30,8 +31,32 @@ class Customs_list(Page):
 
     def __init__(self, *args, **kwargs):
         Page.__init__(self, *args, **kwargs)
-        label = tk.Label(self, text=self.pg_name, font=LARGEFONT)
-        label.pack(side="top", fill="both", expand=True)
+        columns = ("name", "tax_id", "phone", "fax", "address")
+
+        clients_tree = ttk.Treeview(self, columns=columns, show='headings')
+        clients_tree.column("# 1",anchor=tk.W, stretch=tk.NO, width=200)
+        clients_tree.heading("name",text="客戶名稱")
+        clients_tree.column("# 2",anchor=tk.W, stretch=tk.NO, width=100)
+        clients_tree.heading("tax_id",text="統一編號")
+        clients_tree.column("# 3",anchor=tk.W, stretch=tk.NO, width=100)
+        clients_tree.heading("phone",text="電話")
+        clients_tree.column("# 4",anchor=tk.W, stretch=tk.NO, width=100)
+        clients_tree.heading("fax",text="傳真")
+        clients_tree.column("# 5",anchor=tk.W, stretch=tk.NO, width=250)
+        clients_tree.heading("address",text="地址")
+        
+        with Database_Cli(USERS_DATA) as db:
+            tb_clinets = db.read_all_from_table("clients")
+            for row in tb_clinets:
+                print(row)
+                clients_tree.insert("", tk.END, values=row)
+
+        clients_tree.pack()
+
+        scrollbar = ttk.Scrollbar(root, orient=tk.VERTICAL, command=clients_tree.yview)
+        clients_tree.configure(yscroll=scrollbar.set)
+        scrollbar.pack()
+
 
 class MainView(tk.Frame):
     def __init__(self, *args, **kwargs):
@@ -57,7 +82,8 @@ class MainView(tk.Frame):
         b2.pack(side="left")
         b3.pack(side="left")
 
-        p1.show()
+        # p1.show()
+        p3.show()
 
 if __name__ == "__main__":
     root = tk.Tk()
